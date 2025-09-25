28c6760c9105ce04044ad7d3b86a26d1cec0ca34
gery <geryjonathan21@gmail.com>
2025-09-25 22:59:26 +0700

    update gitignore2

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -2,6 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import portfolio from "@/data/portfolio.json"; // import your portfolio data
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+// const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 let chatSessions = {};
